ea2f0032363b56bd8cf769e0c14bccbe0639d061
david-walters <dawudwalters@gmail.com>
2024-08-25 23:38:42 +0100

    Add the necessary configurations to the __init__ file

--- events_list/__init__.py
+++ events_list/__init__.py
@@ -9,6 +9,9 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URL')
+app.config['IP'] = os.getenv('IP', '127.0.0.1')
+app.config['PORT'] = int(os.getenv('PORT', 5000))
+app.config['DEBUG'] = os.getenv('DEBUG', 'False').lower() == 'true'
 
 db = SQLAlchemy(app)
 
