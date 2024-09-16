9ae66d70fa8f90695eadbebe1702ea13dfad78e4
david-walters <dawudwalters@gmail.com>
2024-09-16 22:59:01 +0100

    Apply all that was advised by pylint to the run.py file so that it follows PEP8 standards.

--- run.py
+++ run.py
@@ -1,8 +1,15 @@
+"""
+This module is used to run the Flask application.
+
+It imports the Flask app instance from the events_list module and starts
+the application on the configured host and port.
+"""
+
 from events_list import app
 
 if __name__ == '__main__':
     host = app.config.get('IP', '127.0.0.1')
     port = app.config.get('PORT', 5000)
-    debug = False
-   
-    app.run(host=host, port=port, debug=debug)
+    DEBUG = False
+
+    app.run(host=host, port=port, debug=DEBUG)
